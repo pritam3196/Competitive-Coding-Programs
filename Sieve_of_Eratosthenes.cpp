22dd000a3a58da1f272514b791df1468bfc5d7e5
pritam3196 <pritam6518@gmail.com>
2020-06-27 13:56:53 +0100

    Sieve of Eratosthenes CPP program

---
+++ Sieve_of_Eratosthenes.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+const int Nmax=100001;
+bool isPrime[Nmax];  //here the default value of the bool is false so we have to turn them all to true
+
+void sieveOfEratosthenes(int n){
+    for(int i=2; i<=n;i++)
+        isPrime[i]=true; // here we are making the default value of the array to true9
+    for(int i=2; i<=n/2;i++)
+        if(isPrime[i]==true)
+            for(int j=i*2;j<=n;j+=i)
+                isPrime[j]=false;
+}
+
+
+int main()
+{
+    sieveOfEratosthenes(1);
+    for(int i=2; i<100; i++)
+        if(isPrime[i]==true)
+            cout<< i <<" ";
+    return 0;
+}
